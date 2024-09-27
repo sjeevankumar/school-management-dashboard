161f847d3b363f94e8f6566ded39942f989acf24
sjeevankumar <sjeevankumar89@gmail.com>
2024-09-27 11:58:21 +0530

    feat:add studentpage layout design

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,6 +1,17 @@
+import Announcements from "@/components/Announcements"
+import EventCalendar from "@/components/EventCalendar"
+
 const StudentPage = () => {
   return (
-    <div className=''>StudentPage</div>
+    <div className="p-4 flex gap-4 flex-col xl:flex-row">
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3"></div>
+      {/* RIGHT */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcements />
+      </div>
+    </div>
   )
 }
 
