9122e98ef7a0e0c924d89a0ab94be00daca1533b
sjeevankumar <sjeevankumar89@gmail.com>
2024-09-28 12:05:10 +0530

    feat:add lessonslist page

--- src/app/(dashboard)/list/lessons/page.tsx
+++ src/app/(dashboard)/list/lessons/page.tsx
@@ -1,36 +1,29 @@
 import Pagination from "@/components/Pagination"
 import Table from "@/components/Table"
 import TableSerach from "@/components/TableSerach"
-import { classesData, role, subjectsData } from "@/lib/data"
+import { lessonsData, role } from "@/lib/data"
 import Image from "next/image"
 import Link from "next/link"
 
-type Class = {
+type Lesson = {
   id: number
-  name: string
-  capacity: number
-  grade: number
-  supervisor: string
+  subject: string
+  class: number
+  teacher: number
 }
 
 const columns = [
   {
-    header: "Class Name",
+    header: "Subject Name",
     accessor: "name",
   },
   {
-    header: "Capacity",
-    accessor: "capacity",
-    className: "hidden md:table-cell",
+    header: "Class",
+    accessor: "class",
   },
   {
-    header: "Grade",
-    accessor: "grade",
-    className: "hidden md:table-cell",
-  },
-  {
-    header: "Supervisor",
-    accessor: "supervisor",
+    header: "Teacher",
+    accessor: "teacher",
     className: "hidden md:table-cell",
   },
   {
@@ -39,16 +32,15 @@ const columns = [
   },
 ]
 
-const ClassListPage = () => {
-  const renderRow = (item: Class) => (
+const LessonListPage = () => {
+  const renderRow = (item: Lesson) => (
     <tr
       key={item.id}
       className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
     >
-      <td className="flex items-center gap-4 p-4">{item.name}</td>
-      <td className="hidden md:table-cell">{item.capacity}</td>
-      <td className="hidden md:table-cell">{item.grade}</td>
-      <td className="hidden md:table-cell">{item.supervisor}</td>
+      <td className="flex items-center gap-4 p-4">{item.subject}</td>
+      <td>{item.class}</td>
+      <td className="hidden md:table-cell">{item.teacher}</td>
       <td>
         <div className="flex items-center gap-2">
           <Link href={`/list/teachers/${item.id}`}>
@@ -69,7 +61,7 @@ const ClassListPage = () => {
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       {/* TOP */}
       <div className="flex items-center justify-between">
-        <h1 className="hidden md:block text-lg font-semibold">All Classes</h1>
+        <h1 className="hidden md:block text-lg font-semibold">All Lessons</h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSerach />
           <div className="flex items-center gap-4 self-end">
@@ -88,11 +80,11 @@ const ClassListPage = () => {
         </div>
       </div>
       {/* LIST */}
-      <Table columns={columns} renderRow={renderRow} data={classesData} />
+      <Table columns={columns} renderRow={renderRow} data={lessonsData} />
       {/* PAGINATION */}
       <Pagination />
     </div>
   )
 }
 
-export default ClassListPage
+export default LessonListPage
